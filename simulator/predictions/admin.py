379f6f7bae99d34ddacad8aba611a6dee07a5aeb
eiriniretzepi <ireneretzepis@yahoo.com>
2022-06-27 03:41:21 +0100

    changes in prediction page and watchlist

--- simulator/predictions/admin.py
+++ simulator/predictions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class PredictionsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Predictions, PredictionsAdmin)
--- simulator/predictions/views.py
+++ simulator/predictions/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PredictionForm
+from .models import Predictions
 
 
 def predictions(request):
+    import yfinance as yf
 
-    return render(request, 'predictions.html', {})
+    if request.method == "POST":
+        #should check if the stock already exists SHOULD NOT ADD THE SAME STOCK TWICE
+        form = PredictionForm(request.POST or None)
+
+        if form.is_valid():
+            object = form.save(commit=False)
+            object.user = request.user
+            object.save()
+            return redirect('predictions')
+    else:
+        ticker = Predictions.objects.all()
+        output = []
+        # for ticker_item in ticker:
+        #     tick = yf.Ticker(ticker_item)
+        #     api = tick.info
+        #
+        #     if api.get("currentPrice") is None:
+        #         api = "Error"
+        #     else:
+        #         output.append(api)
+
+        return render(request, 'predictions.html', {'output': output})
+
+ # if request.method == "POST":
+ #        get_ticker = request.POST['ticker']
+ #
+ #        ticker = yf.Ticker(get_ticker)
+ #        api = ticker.info
+ #
+ #        if api.get("currentPrice") is None:
+ #            api = "Error"
 
